bb5dfa4ca385c15ce0cee3a1f3917e7c113189ba
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2024-02-11 23:42:25 +0100

    Add Spigot API as compile-only and test implementation dependency

--- multification-cdn/build.gradle.kts
+++ multification-cdn/build.gradle.kts
@@ -10,4 +10,7 @@ dependencies {
     api(project(":multification-core"))
 
     api("net.dzikoysk:cdn:1.14.4")
+
+    compileOnly("org.spigotmc:spigot-api:1.19.4-R0.1-SNAPSHOT")
+    testImplementation("org.spigotmc:spigot-api:1.19.4-R0.1-SNAPSHOT")
 }
