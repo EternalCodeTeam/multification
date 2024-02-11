d4d3b2ba09dc28f2ae78c09685add2d76d7f3f4b
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2024-02-11 01:09:17 +0100

    Release v1.0.1

--- multification-core/build.gradle.kts
+++ multification-core/build.gradle.kts
@@ -11,5 +11,6 @@ dependencies {
     api("org.jetbrains:annotations:24.1.0")
 
     // TODO: Remove Spigot API, and use kyori in core. for Bukkit, create bukkit-platform.
-    api("org.spigotmc:spigot-api:1.19.4-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.19.4-R0.1-SNAPSHOT")
+    testImplementation("org.spigotmc:spigot-api:1.19.4-R0.1-SNAPSHOT")
 }
