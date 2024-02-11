a36fac65d8a60d5c8c92b80aea8ef0417dbd30d8
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2024-02-11 23:47:36 +0100

    Remove dependencies from build.gradle.kts file

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,11 +6,6 @@ repositories {
     gradlePluginPortal()
 }
 
-dependencies {
-    implementation("net.kyori:indra-git:3.1.3")
-    implementation("com.google.guava:guava:33.0.0-jre")
-}
-
 sourceSets {
     main {
         java.setSrcDirs(emptyList<String>())
